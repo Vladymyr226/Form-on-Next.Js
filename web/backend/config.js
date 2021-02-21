5b7a1fea8c3259e8473ab248a0320c4b65529f80
Vladymyr226 <62399426+Vladymyr226@users.noreply.github.com>
2021-02-21 19:21:30 +0200

    Update config.js
    Setting up a database connection using the environment.

--- web/backend/config.js
+++ web/backend/config.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "BackendFormDataBase",
-  password: "123123",
-  port: 5432,
+  user: process.env.user,
+  host: process.env.host,
+  database: process.env.database,
+  password: process.env.password,
+  port: process.env.port,
   ssl: false,
 });
 
