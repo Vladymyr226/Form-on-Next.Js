26c7915980782a42702735890d3d30e5c68f4e2e
Vladymyr226 <62399426+Vladymyr226@users.noreply.github.com>
2021-02-21 20:59:19 +0200

    Update config.js

--- web/backend/config.js
+++ web/backend/config.js
@@ -2,10 +2,10 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: process.env.user,
-  host: process.env.host,
+  host: "localhost",
   database: process.env.database,
   password: process.env.password,
-  port: process.env.port,
+  port: 5432,
   ssl: false,
 });
 
